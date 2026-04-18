62b88e78bf2aa3f5f3cab65d1b99b8448314cc90
Samuel Dauda <smartsam9981@gmail.com>
2026-04-18 20:02:12 +0100

    sever is live

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ connectDB();
 
 
 // Routes
+app.use('/',(req, res)=>{
+    res.send("Sever is live");
+})
 app.use('/api', productRoute);
 app.use('/api', userRoute);
 app.use('/api', fakeStoreRoute);
